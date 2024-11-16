0cfb90a8a597cd0c4f996c44ab13462c4abe6d25
nguyenquangos0302git <nguyenquangos0302git@gmail.com>
2024-11-16 21:18:28 +0700

    upoload

--- execrise-leetcode/src/Main.java
+++ execrise-leetcode/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String title = "27. Remove Element";
+        System.out.println(modifyTitleIntoClassName(title));
     }
+
+    public static String modifyTitleIntoClassName(String title) {
+        if (title == null || title.isEmpty()) return "";
+        return "_" + title.replaceAll("\\s+|.\\s", "_");
+    }
+
 }
